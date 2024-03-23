35e11524641a44cb68bbfab43180322712ff3fbd
vadim-moshev <reg-vadim-moshev@yandex.ru>
2024-03-23 16:41:17 +0500

    the fisrt commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import Page1 from "./pages/page1";
+import Page2 from "./pages/page2";
+
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route exact path="/" element={<Page1 />} />
+        <Route exact path="/page1" element={<Page1 />} />
+        <Route exact path="/page2" element={<Page2 />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
