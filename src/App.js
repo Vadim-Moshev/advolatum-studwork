dab0a0940b302719f129aa35a3ad928212bf9bde
vadim-moshev <reg-vadim-moshev@yandex.ru>
2024-03-23 19:37:40 +0500

    create main structure

--- src/App.js
+++ src/App.js
@@ -3,18 +3,26 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Page1 from "./pages/page1";
 import Page2 from "./pages/page2";
 
-import logo from "./logo.svg";
 import "./App.css";
+import "./my_styles.css";
 
 function App() {
+  /*
+<BrowserRouter>
+  <Routes>
+    <Route exact path="/" element={<Page1 />} />
+    <Route exact path="/page1" element={<Page1 />} />
+    <Route exact path="/page2" element={<Page2 />} />
+  </Routes>
+</BrowserRouter>
+  */
+
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route exact path="/" element={<Page1 />} />
-        <Route exact path="/page1" element={<Page1 />} />
-        <Route exact path="/page2" element={<Page2 />} />
-      </Routes>
-    </BrowserRouter>
+    <>
+      <header className="header"></header>
+      <main className="main">Через некоторое время тут должен быть сайт</main>
+      <footer className="footer"></footer>
+    </>
   );
 }
 
