85817952791d1803f92d2334d8edd2a67a7523d1
vadim-moshev <reg-vadim-moshev@yandex.ru>
2024-03-23 20:42:26 +0500

    move header to separated component

--- src/App.js
+++ src/App.js
@@ -1,26 +1,25 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-import Page1 from "./pages/page1";
+import MainPage from "./pages/MainPage";
 import Page2 from "./pages/page2";
 
 import "./App.css";
 import "./my_styles.css";
 
 function App() {
-  /*
-<BrowserRouter>
-  <Routes>
-    <Route exact path="/" element={<Page1 />} />
-    <Route exact path="/page1" element={<Page1 />} />
-    <Route exact path="/page2" element={<Page2 />} />
-  </Routes>
-</BrowserRouter>
-  */
-
   return (
     <>
       <header className="header"></header>
-      <main className="main">Через некоторое время тут должен быть сайт</main>
+
+      <BrowserRouter>
+        <main className="main">
+          <Routes>
+            <Route exact path="/" element={<MainPage />} />
+            <Route exact path="/page2" element={<Page2 />} />
+          </Routes>
+        </main>
+      </BrowserRouter>
+
       <footer className="footer"></footer>
     </>
   );
---
+++ src/pages/MainPage/index.jsx
@@ -0,0 +1,15 @@
+import { Link } from "react-router-dom";
+
+import Header from "../../components/header";
+
+import "./index.css";
+
+const MainPage = () => {
+  return (
+    <>
+      <Header />
+    </>
+  );
+};
+
+export default MainPage;
